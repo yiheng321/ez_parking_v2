26016ded3c89be568db4892401bcdb64f85efdd8
ruizhi <ruizhiwang11@gmail.com>
2021-03-25 00:00:36 +0800

    add search page

--- lib/Boundary/MapPage.dart
+++ lib/Boundary/MapPage.dart
@@ -1,21 +1,26 @@
 import 'package:ezparking/Services/Auth.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as LocationManager;
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:ezparking/Boundary/SearchPage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:ezparking/Utils/NavDrawer.dart';
+
+const kGoogleApiKey = "AIzaSyAzedSahYVFaCTK3_YP19NYYd9_mW3EI5A";
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
 
 class MapPage extends StatelessWidget {
   MapPage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
   GoogleMapController mapController;
   LatLng _initialcameraposition = LatLng(1.282302, 103.858528);
-  GoogleMapController _controller;
-  Location _location = Location();
+  final _location = LocationManager.Location();
 
   void _onMapCreated(GoogleMapController _cntlr) {
-    _controller = _cntlr;
+    mapController = _cntlr;
     _location.onLocationChanged.listen((l) {
-      _controller.animateCamera(
+      mapController.animateCamera(
         CameraUpdate.newCameraPosition(
           CameraPosition(target: LatLng(l.latitude, l.longitude), zoom: 15),
         ),
@@ -35,7 +40,10 @@ class MapPage extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-                _ // handlePressButton();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SearchPage()),
+                );
               },)
           ],
         ),
