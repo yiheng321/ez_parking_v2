3715480efb8f47bb70f4f793802dd46e196af632
ruizhi <ruizhiwang11@gmail.com>
2021-04-04 20:08:53 +0800

    refactor

--- lib/Controller/ApiService.dart
+++ lib/Controller/ApiService.dart
@@ -6,8 +6,8 @@ import 'package:http/http.dart' as http;
 class ApiService {
   void loadCarparkList() async {
     var _carparkDB = CarparkDataBase();
-    List<String> _firstList = [];
-    List<int> _secondList = [];
+    List<String> _carparkNoList = [];
+    List<int> _currentsLotsList = [];
     var _response = await http.get(
         Uri.encodeFull(
             "https://api.data.gov.sg/v1/transport/carpark-availability"),
@@ -15,11 +15,11 @@ class ApiService {
     if (_response.statusCode == 200) {
       var jsonBody = await jsonDecode(_response.body) as Map;
       for (var obj in jsonBody["items"][0]["carpark_data"]) {
-        _firstList.add(obj["carpark_number"]);
-        _secondList.add(int.parse(obj["carpark_info"][0]["lots_available"]));
+        _carparkNoList.add(obj["carpark_number"]);
+        _currentsLotsList.add(int.parse(obj["carpark_info"][0]["lots_available"]));
       }
-      Map<String, int> map = Map.fromIterables(_firstList, _secondList);
-      _carparkDB.updateCarParkSlotbyID(map);
+      Map<String, int> carparkInfoMap = Map.fromIterables(_carparkNoList, _currentsLotsList);
+      _carparkDB.updateCarParkSlotbyID(carparkInfoMap);
     }
   }
 
